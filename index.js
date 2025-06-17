db7764c4c4f22478beec203f0221292a8d3aa011
Dhiman-Chandra-Ray <prdhimanchandra@gmail.com>
2025-06-17 21:47:51 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const phones = require('./phones.json');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(cors());
+
+app.get('/',(req, res) => {
+    res.send('my phones is comming soon');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.get('/phones', (req, res) => {
+    res.send(phones);
 });
+
+app.listen(port, ()=> {
+    console.log(`Server is running on http://localhost:${port}`);
+})
